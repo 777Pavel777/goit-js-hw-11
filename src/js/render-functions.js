e3324a0d452f93511f60c3292b450575b4a4e60d
777Pavel777 <cross1295@icloud.com>
2024-03-02 18:16:57 +0200

    rename project

---
+++ src/js/render-functions.js
@@ -0,0 +1,29 @@
+export function createMarkup(images) {
+  const markupImg = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<li class="image-search">
+        <a href="${largeImageURL}">
+        <img class="gallery-img" src="${webformatURL}" alt="${tags}"/>
+        </a>
+        <ul class="img-info">
+          <li class="img-items">
+            <p class="img-value"><span class="img-subtitle">Likes</span>${likes}</p>
+            <p class="img-value"><span class="img-subtitle">Views</span>${views}</p>
+            <p class="img-value"><span class="img-subtitle">Comments</span>${comments}</p>
+            <p class="img-value"><span class="img-subtitle">Downloads</span>${downloads}</p>
+          </li>
+        </ul>
+      </li>`;
+      }
+    )
+    .join('');
+}
